cd45e842df826e425b9b32f5294e876f6ff270fb
ian schoonover <iantskon@gmail.com>
2015-05-27 10:28:58 +0200

    Finish adding comments and styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+	def create
+	    @treasure = Treasure.find(params[:treasure_id])
+	    @comment = @treasure.comments.create(comment_params)
+	    redirect_to treasure_path(@treasure)
+	  end
+	 
+	  private
+	    def comment_params
+	      params.require(:comment).permit(:bru, :comment)
+	    end
 end
