5dae3ad4518c3e3c266d9456f6e38e71d19770d8
ian schoonover <iantskon@gmail.com>
2015-05-27 10:08:45 +0200

    Add validations to treasure and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :treasure
+  validates :bru, presence: true,
+                      length: { minimum: 2 }
+  validates :comment, presence: true,
+                      length: { minimum: 8 }
 end
--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,3 +1,7 @@
 class Treasure < ActiveRecord::Base
 	has_many :comments
+	validates :title, presence: true,
+	                    length: { minimum: 5 }
+	validates :description, presence: true,
+	                    length: { minimum: 8 }
 end
